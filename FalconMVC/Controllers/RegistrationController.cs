fcf8d7ad21389199c48a075c7b35544591565faa
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2021-05-13 22:37:43 +0300

    feat : add Registration

---
+++ FalconMVC/Controllers/RegistrationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FalconMVC.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ FalconMVC/ViewModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FalconMVC.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email : ")]
+        [Required(ErrorMessage = "Email address is required.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        
+        [Display(Name = "Password : ")]
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
