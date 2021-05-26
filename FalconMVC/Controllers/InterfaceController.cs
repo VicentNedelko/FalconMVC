eaeecd45babdc7209619c29d0cae1218b524f2f3
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2021-05-26 18:13:12 +0300

    feat : add Interface discover method

---
+++ FalconMVC/Controllers/InterfaceController.cs
@@ -0,0 +1,25 @@
+﻿using Knx.Bus.Common.KnxIp;
+using Knx.Falcon.Sdk;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FalconMVC.Controllers
+{
+    public class InterfaceController : Controller
+    {
+        public IActionResult ShowAll()
+        {
+            return View(GetInterfacesList());
+        }
+
+        private DiscoveryResult[] GetInterfacesList()
+        {
+            DiscoveryClient discoveryClient = new DiscoveryClient(adapterType: AdapterTypes.All);
+            DiscoveryResult[] results = discoveryClient.Discover();
+            return results;
+        }
+    }
+}
