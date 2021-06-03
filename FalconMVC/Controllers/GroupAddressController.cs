2c84168556eb3b34a56f80cda15b5ed70c2ac8e0
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2021-06-03 23:38:55 +0300

    fix : AsNoTracking add

--- FalconMVC/Controllers/GroupAddressController.cs
+++ FalconMVC/Controllers/GroupAddressController.cs
@@ -14,7 +14,7 @@ namespace FalconMVC.Controllers
     public class GroupAddressController : Controller
     {
         private readonly DbFalcon _dbFalcon;
-        private Regex regex =
+        private readonly Regex _regex =
             new Regex(@"^([0-9]|[1-9][0-9]|[1-2][0-5][0-5]){1}\/([0-9]|[1-9][0-9]|[1-2][0-5][0-5]{1})\/([0-9]|[1-9][0-9]|[1-2][0-5][0-5]){1}$");
         public GroupAddressController(DbFalcon dbFalcon)
         {
@@ -30,7 +30,7 @@ namespace FalconMVC.Controllers
         [HttpGet]
         public async Task<IActionResult> AddArchiveAsync()
         {
-            return View(await _dbFalcon.GAs.ToListAsync());
+            return View(await _dbFalcon.GAs.AsNoTracking().ToListAsync());
         }
 
         [HttpPost]
@@ -38,16 +38,16 @@ namespace FalconMVC.Controllers
         {
             if(nameGA is not null && !_dbFalcon.GAs.Any(ga => ga.GAddress == nameGA))
             {
-                if (regex.IsMatch(nameGA))
+                if (_regex.IsMatch(nameGA))
                 {
                     var result = await _dbFalcon.GAs.AddAsync(new GA { GAddress = nameGA, GType = typeGA });
                     if (result.State == EntityState.Added)
                     {
                         await _dbFalcon.SaveChangesAsync();
-                        return View(await _dbFalcon.GAs.ToListAsync());
+                        return View(await _dbFalcon.GAs.AsNoTracking().ToListAsync());
                     }
                 }
-                ViewBag.Error = "GA doesn't correspond the pattern.";
+                ViewBag.Error = "GA doesn't correspond the 3-level pattern - __/__/__.";
                 return View("Error");
             };
             return Content("GA is also exist or equal to null.");
