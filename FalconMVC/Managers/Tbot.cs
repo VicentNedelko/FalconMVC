4fa1f4eb220e59dcb978cb70cfce67ef7b2ea05e
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2021-07-08 15:55:40 +0300

    feat : add GetChatId method

--- FalconMVC/Managers/Tbot.cs
+++ FalconMVC/Managers/Tbot.cs
@@ -1,7 +1,10 @@
 ﻿using FalconMVC.Globals;
+using FalconMVC.Models.BotModels;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
@@ -12,13 +15,24 @@ namespace FalconMVC.Managers
     public class Tbot : IBot
     {
         private static ITelegramBotClient telegramBotClient;
+        private readonly string baseUri = "https://api.telegram.org/";
         public async Task SendMessageAsync(string message)
         {
             telegramBotClient = new TelegramBotClient(token: Secret.Tbot);
             _ = await telegramBotClient.SendTextMessageAsync(
-                chatId: 481679093,
+                chatId: await GetChatIdAsync(),
                 text: message,
                 disableNotification: false);
+        }
+
+        public async Task<long> GetChatIdAsync()
+        {
+            string reqPath = String.Concat(baseUri, "bot", Secret.Tbot, "/getUpdates");
+            Uri request = new(reqPath);
+            HttpClient httpClient = new();
+            var responce = await httpClient.GetAsync(request);
+            var updateResponce = JsonConvert.DeserializeObject<Root>(await responce.Content.ReadAsStringAsync());
+            return updateResponce.Result.FirstOrDefault().Message.Chat.Id;
         }
 
     }
---
+++ FalconMVC/Models/BotModels/Root.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FalconMVC.Models.BotModels
+{
+    public class Root
+    {
+        public bool Ok { get; set; }
+        public List<Updates> Result { get; set; }
+    }
+}
